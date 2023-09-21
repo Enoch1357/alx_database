00c128a388785d05288ea360606da2841a3f89dc
Enoch1357 <enochmensah1357@gmail.com>
2023-09-21 17:43:13 +0000

    2-create_read_user.sql update

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,3 +3,4 @@ CREATE SCHEMA IF NOT EXITS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2;
 SET PASSWORD FOR 'user_0d_2'@'localhost'="user_0d_2_pwd";
+GRANT ALL PRIVILEGES ON *.* FOR 'user_0d_2'@'localhost';
