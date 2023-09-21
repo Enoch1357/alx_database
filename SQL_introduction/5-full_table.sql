55fb9efff00df5682d24243acde07dacfb4eb1f3
Enoch1357 <enochmensah1357@gmail.com>
2023-09-21 03:45:27 +0000

    5-full_table.sql update

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- prints the full description of the table 'first_table' from the database 'hbtn_0c_0'
-SHOW COLUMNS  FROM first_table;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_COMMENT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
