f4fb79283178662eb47c793cbfad93bb57345505
Enoch1357 <enochmensah1357@gmail.com>
2023-09-21 03:28:44 +0000

    9-full_creation.sql update

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
 -- Creates a table 'second_table' in the database 'hbtn_0c_0' and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table
 (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     score INT
 );
 
-INSERT INTO second_table(name, score) VALUES 
-    ("John", 10),
-    ("Alex", 3),
-    ("Bob", 14),
-    ("George", 8);
+INSERT INTO second_table(id, name, score) VALUES 
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
