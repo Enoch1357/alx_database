d6a6ecbdd9dc52e75391b425adcd15b28df851a5
Enoch1357 <enochmensah1357@gmail.com>
2023-09-21 17:47:03 +0000

    2-create_read_user.sql update

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Creating the database 'hbtn_0d_2' and the user 'user_0d_2'
 CREATE SCHEMA IF NOT EXITS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2;
+GRANT SELECT ON 'hbtn_0d_2'.* TO user_0d_2;
 SET PASSWORD FOR 'user_0d_2'@'localhost'="user_0d_2_pwd";
-GRANT ALL PRIVILEGES ON *.* FOR 'user_0d_2'@'localhost';
